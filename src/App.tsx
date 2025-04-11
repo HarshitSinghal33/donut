380d556b8371cdb83d54d80626ce28acbf032ff0
Harshit Singhal <harshitsinghal483@gmail.com>
2025-04-11 21:33:54 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import DonutViewer from "./pages/Donought";
+// import GoalTimeline from "./pages/GoalTimeLine";
+
+function App() {
+  return (
+    <>
+      {/* <GoalTimeline /> */}
+      <DonutViewer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/Donought.tsx
@@ -0,0 +1,21 @@
+import { Suspense } from 'react';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls, useGLTF } from '@react-three/drei';
+
+function DonutModel() {
+  const { scene } = useGLTF('/donut.glb');
+  return <primitive object={scene} scale={1} />
+}
+
+export default function DonutViewer() {
+  return (
+    <Canvas camera={{ position: [0, 0, 10], fov: 30 }} style={{ width: '100%', height: '100vh' }}>
+      <ambientLight intensity={0.75} />
+      <directionalLight position={[5, 5, 5]} />
+      <Suspense fallback={null}>
+        <DonutModel />
+      </Suspense>
+      <OrbitControls enableZoom={true} />
+    </Canvas>
+  );
+}
